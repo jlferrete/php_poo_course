f89a801e88d4e7d2c9c066e6895c31b6783f2179
Jose Luis Ferrete <joseluisferrete@gmail.com>
2018-08-05 10:47:28 +0200

    Private and protected

---
+++ private_protected.php
@@ -0,0 +1,41 @@
+<?php 
+
+class BaseProfile
+{
+    protected $connection_data = 'Conexion BD';
+    protected function connect2DB()
+    {
+        echo $this->connection_data;
+    }
+    public function __construct()
+    {
+        $this->connect2DB();
+    }
+}
+
+class MyProfile extends BaseProfile
+{
+
+   
+    public $name;
+    public $last_name;
+
+    public function __construct()
+    {
+        $this->connect2DB();
+    }
+
+     public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        //Reglas para la asignación de nuestro correo
+        $this ->email = $email;
+    }
+
+}
+
+$instBase = new MyProfile;
